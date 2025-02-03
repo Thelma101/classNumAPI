18916b02dda5520dd46edba5f3cba82ff0c1254b
Thelma101 <akpata.thelma@gmail.com>
2025-02-03 17:51:00 +0100

    typescript type casting

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const axios = require('axios');
+// const dotenv = require('dotenv');
+// const api = require('./index');
+import express, {request, response} from 'express' ;
 const app = express();
-const cors = require('cors');
-const axios = require('axios');
-const dotenv = require('dotenv');
-const api = require('./index');
+import axios from 'axios';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import api from './index';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+
+app.get('/', req,, res) => {
+    const numberParam = req.query.number;
+}
+
 const isPrime = (num: number): boolean => {
     if (num <= 1) return false;
     for (let i = 2; i <= Math.sqrt(num); i++) {
@@ -9,7 +14,7 @@ const isPrime = (num: number): boolean => {
 const isPerfect = (num: number): boolean => {
     if (num <= 1) return false;
     let sum = 1;
-    for(let i= 2; i <= Math.sqrt(num); i++ ) {
+    for (let i= 2; i <= Math.sqrt(num); i++ ) {
         if (num % i === 0) {
             sum += i;
             if (i !== sum / i) sum += num /i
@@ -23,11 +28,10 @@ const isPerfect = (num: number): boolean => {
 //         acc + Math.pow(Number(curr), 3), 0);
 // }
 
-const isAmrmstrong = (num: nuumber): boolean => {
+const isAmrmstrong = (num: number): boolean => {
     const inputs = String(num).split('');
-    const sum = inputs.reduce((prev, curr) => {
-        return prev + Math.pow(Number(curr), inputs.length);
-    })
+    const sum = inputs.reduce((prev, curr) => prev + Math.pow(Number(curr), inputs.length), 0);
+    return sum === num;
 }
 
 console.log(isAmrmstrong(371)); // true
