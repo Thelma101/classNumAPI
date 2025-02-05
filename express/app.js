68e19579867c3bddd03ed80291f02e1245df6318
Thelma101 <akpata.thelma@gmail.com>
2025-02-05 15:51:35 +0100

    final working commit: thirdparty endpoint integration

--- express/app.js
+++
@@ -1,9 +0,0 @@
-import app from './index.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-    console.log(`Application Server is running on PORT: ${PORT}`);
-});
